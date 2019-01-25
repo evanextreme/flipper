73144414ee1eeaaf1de68e440f55b50680ad289b
Evan Hirsh <evanx11@gmail.com>
2019-01-24 21:42:32 -0500

    done for the day kill me

--- flipper/flipper.py
+++ flipper/flipper.py
@@ -2,7 +2,10 @@ import requests
 import re
 import os
 import argparse
+import spacy
 from bs4 import BeautifulSoup as BS
+from halo import Halo
+from spacy.tokens import Doc
 
 def cli():
     parser = argparse.ArgumentParser(description='A web scraping tool to help pentesters crawl websites for vulnerable personal info')
@@ -10,37 +13,66 @@ def cli():
                         help='url(s) to crawl for vulnerabilities')
     parser.add_argument('-r', dest='recursive', default=False, action="store_true", 
                         help='recursively crawl all top level URLs found on the requested site')
+    parser.add_argument('--debug', dest='debug', default=False, action="store_true", 
+                    help='Debugging is actually all about finding the bug. About understanding why the bug was there to begin with. About knowing that it’s existence was no accident.')
     args = parser.parse_args()
 
-    vulns = {}
-    cleaned_urls = add_protocols(args.url)
-    for url in cleaned_urls:
-        scraper = get_scraper(url)
-        found_strings, found_urls = format_site(scraper)
-        vuln_items = crawl_emails(found_strings + found_urls)
-        for s in vuln_items:
-            if s not in vulns:
-                vulns[s] = {'located_on': []}
-            vulns[s]['located_on'] += url
-        if args.recursive:
-            cleaned_found_urls = add_protocols(found_urls)
-            for found_url in cleaned_found_urls:
-                if found_url not in cleaned_urls:
-                    cleaned_urls += found_url
-            print(cleaned_found_urls)
-    print(vulns)
+    emails = ()
+    phones = ()
+    ents = ()
+
+    spinner = Halo(text='GET HYPED', spinner='dots')
+    spinner.start()
+    for base_url in args.url:
+        cleaned_urls = [base_url]
+        for url in cleaned_urls:
+            
+            spinner.text = 'Scraping {}'.format(url)
+            # build a beautiful soup web scraper
+            scraper = get_scraper(url)
+            # pull strings, urls, and body text from the current url
+            found_strings, found_urls, text = format_site(scraper)
+            # run website page through nlp, build and return "entities" of people / orgs
+            ents = set(find_ents(scraper)).union(ents)
+            # build entity class in vuln dict if entity not already present
+
+            emails = set(crawl_emails(text.split() + found_urls)).union(emails)
+            phones = set(crawl_phones(text.split() + found_urls)).union(phones)
+
+            if args.recursive == True:
+                cleaned_found_urls = add_protocols(base_url, found_urls)
+                for found_url in cleaned_found_urls:
+                    if found_url not in cleaned_urls and base_url in found_url:
+                        cleaned_urls.append(found_url)
+
+    vectored_emails = []
+    for email in emails:
+        spinner.text = 'Vectoring {}'.format(email)
+        vector_entities(email, ents)
+
+    print(emails)
+    print(phones)
+    print(ents)
 
 def print_title(title):
     rows, columns = os.popen('stty size', 'r').read().split()
     columns = int(cols) - len(title)
     print('-' * round(columns/2) + title + '-' * round(columns/2))
 
-def add_protocols(urls):
-    r = re.compile('^(http|https):\/\/')
+def add_protocols(base, urls):
+    r = re.compile('^\/([a-zA-Z0-9])+')
     for i, url in enumerate(urls):
-        if not r.match(url):
-            urls[i] = 'http://' + url
+        if r.match(url):
+            urls[i] = base + url[1:]
     return urls
+
+def vector_entities(data, ents):
+    wordlist = [data] + [x.text for x in ents]
+    nlp = spacy.load('en_core_web_md')  # make sure to use larger model!
+    tokens = Doc(nlp, wordlist)
+    for token in tokens[1:]:
+        print(tokens[0].text, token.text, tokens[0].similarity(token))
+
 
 def get_scraper(url):
     r = requests.get(url)
@@ -48,15 +80,31 @@ def get_scraper(url):
     return bs
 
 def format_site(bs):
+    text = bs.get_text()
+    strings = bs.find_all('a')
     urls = [x.get('href') for x in bs.find_all('a')]
-    strings = re.sub('\n', ' ', bs.get_text()).split()
-    
-    return strings, urls
+
+    return strings, urls, text
+
+def find_ents(bs):
+    body_text = ' '.join(
+        [re.sub(r'(\n|[ ]{2,})', ' ', x.get_text()) for x in bs.find_all('body')])
+    nlp = spacy.load('en_core_web_md')
+    doc = nlp(body_text)
+    ents = [x for x in doc.ents if x.label_ == 'PERSON' or x.label_ == 'ORG']
+
+    return ents
 
 def crawl_emails(strings):
-    r = re.compile('([a-zA-Z0-9])+@([a-zA-Z0-9])+\.(com|org|net|edu)')
-    emails = filter(r.search, strings)
-    return emails
+    remail = re.compile('([a-zA-Z0-9])+@([a-zA-Z0-9])+\.(com|org|net|edu)')
+    vulns = list(filter(remail.search, strings))
+    return vulns
+
+def crawl_phones(strings):
+    rphone = re.compile(
+            '^([0-9](-))?(\(?[0-9]{3}\)?|[0-9]{3})(-)([0-9]{3}(-)[0-9]{4}|[a-zA-Z0-9]{7})$')
+    vulns = list(filter(rphone.search, strings))
+    return vulns
 
 if __name__ == '__main__':
     cli()
